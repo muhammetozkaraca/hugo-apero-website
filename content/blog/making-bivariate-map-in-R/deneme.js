9007f5f674bd875832838e702ad7a7c2e3dab7a6
muhammetozkaraca <muhammetozk@icloud.com>
2023-05-23 23:25:37 +0300

    blog-making-bivariate-maps-in-R

---
+++ content/blog/making-bivariate-map-in-R/deneme.js
@@ -0,0 +1,10 @@
+<script>
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+}
+</script>
